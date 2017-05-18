0708411ad2b0192261df5eb4b3296f5f17619a6e
haoSeven <805615689@qq.com>
2017-05-18 23:51:15 +0800

    完善了添加收藏以后收藏数量增长逻辑

--- apps/operation/views.py
+++ apps/operation/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 from .models import UserFavorite, CourseComments
 from course.models import Course
+from organization.models import Teacher, CourseOrg
 
 
 class AddFavView(View):
@@ -12,16 +13,31 @@ class AddFavView(View):
     机构收藏, 取消收藏
     """
     def post(self, request):
-        fav_id = request.POST.get("fav_id", 0)
-        fav_type = request.POST.get("fav_type", 0)
+        fav_id = int(request.POST.get("fav_id", 0))
+        fav_type = int(request.POST.get("fav_type", 0))
 
         if not request.user.is_authenticated():
             # 判断用户登录状态
             return HttpResponse('{"status":"fail", "msg":"用户未登录"}', content_type="application/json")
 
-        exist_records = UserFavorite.objects.filter(user=request.user, fav_id=int(fav_id), fav_type=int(fav_type))
+        exist_records = UserFavorite.objects.filter(user=request.user, fav_id=fav_id, fav_type=fav_type)
         if exist_records:
             # 用户收藏记录已存在，表示用户要取消收藏
+            if fav_type == 1:
+                course = Course.objects.get(id=fav_id)
+                if course.fav_nums > 0:
+                    course.fav_nums -= 1
+                    course.save()
+            elif fav_type == 2:
+                teacher = Teacher.objects.get(id=fav_id)
+                if teacher.fav_nums > 0:
+                    teacher.fav_nums -= 1
+                    teacher.save()
+            elif fav_type == 3:
+                org = CourseOrg.objects.get(id=fav_id)
+                if org.fav_nums > 0:
+                    org.fav_nums -= 1
+                    org.save()
             exist_records.delete()
             return HttpResponse('{"status":"fail", "msg":"收藏"}', content_type="application/json")
         else:
@@ -31,6 +47,18 @@ class AddFavView(View):
                 user_fav.fav_type = int(fav_type)
                 user_fav.user = request.user
                 user_fav.save()
+                if fav_type == 1:
+                    course = Course.objects.get(id=fav_id)
+                    course.fav_nums += 1
+                    course.save()
+                elif fav_type == 2:
+                    teacher = Teacher.objects.get(id=fav_id)
+                    teacher.fav_nums += 1
+                    teacher.save()
+                elif fav_type == 3:
+                    org = CourseOrg.objects.get(id=fav_id)
+                    org.fav_nums += 1
+                    org.save()
                 return HttpResponse('{"status":"success", "msg":"已收藏"}', content_type="application/json")
             else:
                 return HttpResponse('{"status":"fail", "msg":"收藏错误"}', content_type="application/json")
